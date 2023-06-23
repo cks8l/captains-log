cbb944419896bad13fcc2e5268e52e484f318c86
caleb <cks8l@outlook.com>
2023-06-23 16:43:54 -0400

    Continuing tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+        <p>Cool2</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>About</title>;
+
+export default AboutPage;
