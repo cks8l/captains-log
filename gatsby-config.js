da0f6374deec97872f566c4bdced637b6f0469c7
caleb <cks8l@outlook.com>
2023-06-23 14:19:10 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Captains Log`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
