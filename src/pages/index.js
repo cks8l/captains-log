fe51b385c959ea5ae5285a57d0a159747b57674d
caleb <cks8l@outlook.com>
2023-06-26 14:40:17 -0400

    Continuing Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>Cool</p>
+      <StaticImage
+        alt="Metroparks Toledo's green roof"
+        src="../images/464.jpg"
+      ></StaticImage>
     </Layout>
   );
 };
