16f06cf47f10a98881330d2b11748defc49f4364
caleb <cks8l@outlook.com>
2023-06-27 13:30:26 -0400

    Continuing Gatsby tutorial

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,22 +4,30 @@ import Seo from "../components/seo";
 import { graphql } from "gatsby";
 
 const BlogPage = ({ data }) => {
+  console.log(data);
   return (
     <Layout pageTitle="Blog">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
